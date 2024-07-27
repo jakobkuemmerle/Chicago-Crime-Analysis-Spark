551945f26254ffcc848b21ee4f204f34dd82b31c
Jakob Kuemmerle <145155925+jakobkuemmerle@users.noreply.github.com>
2024-07-27 12:00:48 +0200

    aggregate by time

--- Temporal-Patterns/temporal-analysis.py
+++ Temporal-Patterns/temporal-analysis.py
@@ -21,11 +21,26 @@ def process_data(crime_data):
     crime_data = crime_data.withColumn('DayOfWeek', dayofweek(crime_data['Timestamp']))
     return crime_data.withColumn('Month', month(crime_data['Timestamp']))
 
+def filter_and_group(data_frame, column):
+    """Filter arrests data and group by specified column."""
+    arrests_data = data_frame.filter(data_frame['Arrest'] == 'true')
+    return arrests_data.groupBy(column).count().orderBy(column)
+
 def main():
     # Load and process the crime data
     crime_data_path = "hdfs://wolf:9000/user/uhw4967/crime_data/Crimes_-_2001_to_present.csv"
     crime_data = load_data(crime_data_path)
     processed_data = process_data(crime_data)
+
+    # Aggregate data by hour, day of week, and month
+    hourly_data = filter_and_group(processed_data, 'Hour')
+    weekly_data = filter_and_group(processed_data, 'DayOfWeek')
+    monthly_data = filter_and_group(processed_data, 'Month')
+    
+    # Convert Spark DataFrames to pandas DataFrames for plotting and saving
+    hourly_data_pd = hourly_data.toPandas()
+    weekly_data_pd = weekly_data.toPandas()
+    monthly_data_pd = monthly_data.toPandas()
 
 
 if __name__ == "__main__":
